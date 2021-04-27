e675541fa5396bc348de0444557156a7dd85f7dd
Gladstone <gladstone.compras@gmail.com>
2021-04-26 22:18:35 -0300

    Atualização

--- EDL_WK2_Data_Wrangling.R
+++ EDL_WK2_Data_Wrangling.R
@@ -2,10 +2,14 @@
 # 26/04/2021
 # Essentials of Data Literacy Week 2 - Activity 1
 # Data Wrangling with the College Scorecard Dataset
+# Our research question
+# At every step, we should review our research question. A good research question for this dataset is:
+# Which factors are related to the cost of attendance?
+
 setwd("C:/Users/GCM/Documents/GitHub/Essentials-of-Data-Literacy-EDX")
 getwd()
 library(tidyverse)
-who <- read.csv("Datasets/world_health_organization.csv")
+who <- read.csv("datasets/world_health_organization.csv")
 glimpse(who)
 head(who)
 head(who, 7)
