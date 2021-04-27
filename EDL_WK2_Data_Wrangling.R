ebe866436b266677eef3d206b6b94427c6d60674
Gladstone <gladstone.compras@gmail.com>
2021-04-26 22:23:50 -0300

    Update EDL_WK2_Data_Wrangling.R

--- EDL_WK2_Data_Wrangling.R
+++ EDL_WK2_Data_Wrangling.R
@@ -6,13 +6,53 @@
 # At every step, we should review our research question. A good research question for this dataset is:
 # Which factors are related to the cost of attendance?
 
+rm(list = ls()) #limpa variáveis
 setwd("C:/Users/GCM/Documents/GitHub/Essentials-of-Data-Literacy-EDX")
 getwd()
 library(tidyverse)
-who <- read.csv("datasets/world_health_organization.csv")
-glimpse(who)
-head(who)
-head(who, 7)
-tail(who)
-tail(who,7)
+college_sc <- read_csv("datasets/college_scorecard_data.csv")
 
+# Ask some questions
+#When you first import the dataset, you should ask yourself some basic questions. For example:
+#  How many rows and columns are in the dataset?
+#  How many numeric variables are there?
+#  How many character variables are there?
+#  Are other variable types represented?
+#  Do the data values seem to match up with the variable type?
+#  What does each row of data represent?
+#  Can we tell if any rows of data contain missing values?
+
+college_sc
+glimpse(college_sc)
+View(college_sc)
+head(college_sc, 6)
+tail(college_sc, 6) 
+
+# Wrangling column names
+names(college_sc)
+
+# Changing the name of a variable
+college_sc <- college_sc %>% rename(state = STABBR)
+names(college_sc)
+
+# Changing the name/appearance of multiple variables
+names(college_sc) <- str_to_lower(names(college_sc))
+names(college_sc)
+
+#Wrangling data values
+summary(college_sc)
+
+# Changing the format of a variable
+college_sc <- college_sc %>% 
+  mutate(adm_rate = as.numeric(str_replace(adm_rate, "NULL", "")))
+
+# Creating a new variable and transforming existing variables
+# Run this first
+college_sc <- college_sc %>%
+  mutate(avg_facsal_year = avgfacsal*12)
+# Run this second
+college_sc %>% 
+  select(instnm, avgfacsal, avg_facsal_year)
+
+# Wrapping up
+write_csv(college_sc, "datasets/college_sc_cleaned.csv")
